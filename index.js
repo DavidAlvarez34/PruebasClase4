df12d493b156fb9681ad599dda6c2efb3a6efebe
DavidAlvarez34 <davidalva963@gmail.com>
2021-08-19 19:58:27 -0500

    tercer cambio

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-function hola(params){
-    return "Hola"
+/*function llamada(){
+    console.log("Hola Mundo");
 }
+//funcion que llama a otra funcion
+function llamadora(call){
+    console.log("Dentro de esta funcion llamamos nuestro callback");
+    call();//ejecuta la funcion llamada
+}
+llamadora(llamada);*/
+//argumentos de funciones
+console.log("Este es un texto va primero");
+//asincronia- causa y efecto casualidad
+setTimeout(()=>{
+    console.log("Este texto va segundo");
+},6000);
+setTimeout(()=>{
+    console.log("Este texto va tercero");
+},2000);
+console.log("Este va cuarto");
+//cascada
